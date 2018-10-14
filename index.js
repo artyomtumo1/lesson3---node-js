ff2edb7bc510248ad4f5db1fd40a1f8e860ea25b
Your Name <you@example.com>
2018-10-14 13:48:38 +0400

    des

--- index.js
+++ index.js
@@ -35,6 +35,21 @@
 // });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //1// 
 //console.log("Hello Node");
 
@@ -64,6 +79,7 @@
 // app.listen(3000, function(){
 //    console.log("Example is running on port 3000");
 // });
+
 //4//
 // var express = require("express");
 // var app = express();
@@ -100,17 +116,106 @@
 //     console.log("Example is running on port 3000");
 // });
 
+
+
+
+
+
+
+
 //home work 2//
 
-// var express = require("express");
-// var app = express();
+var express = require("express");
+var app = express();
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
-// app.get("/", function(req, res){
-//    res.redirect("index.html");
-// });
+app.get("/", function(req, res){
+   res.redirect("index.html");
+});
 
-// app.listen(3000, function(){
-//    console.log("Example is running on port 3000");
-// });
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
+
+
+
+
+
+
+
+
+//page 18//
+
+
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+
+//page 22
+// var fs = require('fs');
+
+// function main(){
+//     var file = "hello.txt"
+//     fs.appendFileSync(file,"Hello world\n")
+// }
+// main();
+
+//page23 /1
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+
+//page23 /2
+// var fs = require('fs');
+
+// function main() {
+//    fs.writeFileSync("hello.txt", "Hello world\n");
+//    console.log("fs.writeFile");
+// }
+// main();
+
+//page 24
+//fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//     fs.writeFileSync("dummytext.txt", dummyText)
+//     var text = fs.readFileSync("dummytext.txt").toString();
+//     console.log(dummyText == text);
+//     console.log(text);
+//     fs.writeFileSync("undummytext.txt",
+//         text.replace("Apple","Microsoft")
+       
+//     );
+    
+// }
+// main();
+
+//page25/homework
+// fs = require('fs');
+
+// function TigranPomoshnik(){
+//   var obj =
+//     {
+//         "first_name":"vardan",
+//         "last_name":"Hovsepyan",
+//         "age":13,
+//         "tumo_student":true
+//     }
+//     var stringObj = JSON.stringify(obj);
+//     fs.writeFileSync("obj.json",stringObj)
+//     console.log("you not to idiot")
+// }
+// TigranPomoshnik()
+// console.log("you idiot")
---
+++ module.js
@@ -0,0 +1,13 @@
+//page 18
+// module.exports = class Square {
+//                                   constructor(side) 
+//                                   {
+//                                       this.side = side
+//                                   }
+//                                   getArea() 
+//                                   {
+//                                       return this.side * this.side;
+//                                   }
+//                                }
+
+//page 22
